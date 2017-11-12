e3ddfc1a6fdfa23ad9f66a784531a0520c624f42
Martin Tournoij <martin@arp242.net>
2017-11-12 02:08:31 +0000

    Fix example

--- example_test.go
+++ example_test.go
@@ -16,15 +16,21 @@ func Example() {
 	})
 	ctx := context.Background()
 
+	msg := strings.NewReader("Subject: Hello\r\n\r\nHey there!\r\n")
+
 	// Check if a message is spam.
-	report, err := c.Check(ctx, strings.NewReader("Hello"), nil)
+	check, err := c.Check(ctx, msg, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(report.Score)
+	fmt.Println(check.Score)
 
 	// Report ham for training.
-	c.Tell(ctx, msg, Header{}.
+	tell, err := c.Tell(ctx, msg, Header{}.
 		Set("Message-class", "ham").
 		Set("Set", "local"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(tell)
 }
